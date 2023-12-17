aaf035669b1eb7730453ac085a3e40b4dc809cbb
Kevin <k_le@outlook.com>
2023-12-17 01:02:02 -0600

    output export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
         config.resolve.alias.canvas = false
         config.resolve.alias.encoding = false
